f44cc9a2ab186b1a9801bbb232bba5b9511d92b3
Hannah Dwan <hannahdwan96@gmail.com>
2017-06-20 16:54:55 +0100

    Preliminary DM system for setting up your own blacklist

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ client = discord.Client()
 
 async def public_vague_callout(message, client):
 	print(message.content)
-	await client.send_message(message.channel, 'Please don\'t say dude.')
+	await client.send_message(message.channel, 'Please don\'t say dude or guys!')
 
 @client.event
 async def on_ready():
@@ -15,14 +15,23 @@ async def on_ready():
 @client.event
 async def on_message(message):
 	if message.author.id != client.user.id:
-		if 'dude' in message.content:
+		if 'dude' in message.content or 'guys' in message.content:
 			print(message.author.id)
 			await public_vague_callout(message, client)
 
 @client.event
 async def on_server_join(server):
 	serverOwner = server.owner 
-	await client.send_message(serverOwner, 'I\'ve just been invited to your server! Hi!')
+	await client.send_message(serverOwner, 'I\'ve just been invited to your server! Hi! I just need to run through some things with you!')
+	await client.send_message(serverOwner, 'First of all, we need to decide what words you want me to call out. By default, we include "dude" and "guys", but you\'re able to add any more if you want.')
+	await client.send_message(serverOwner, 'Do you want to add any more blacklisted words? Y/N')
+	def check(msg):
+		if msg.content == 'Y' or msg.content == 'N':
+			return True
+		else:
+			return False
+	newMessage = await client.wait_for_message(author=serverOwner, check=check)
+	print(newMessage)
 
 client.run('TOKEN')
 
